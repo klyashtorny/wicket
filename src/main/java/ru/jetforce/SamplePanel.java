1e2aa70fff6d9a470288f2cda25a931af0088ea3
Anton Kliashtorny <lm_1@tut.by>
2018-02-22 15:10:24 +0300

    implement WelcomePage from SamplePanel

---
+++ src/main/java/ru/jetforce/SamplePanel.java
@@ -0,0 +1,54 @@
+package ru.jetforce;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.PropertyModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SamplePanel extends Panel {
+    private List<String> genderChoices = new ArrayList<>();
+
+    public SamplePanel(String id) {
+        super(id);
+        genderChoices.add("Male");
+        genderChoices.add("Female");
+        final User user = new User();
+        Form<?> form = new Form("form");
+        final TextField<String> text = new TextField<String>("text", new PropertyModel<String>(user, "name"));
+        text.setOutputMarkupId(true);
+        final DropDownChoice<String> gender = new DropDownChoice<String>("gender",
+                new PropertyModel<String>(user, "gender"), genderChoices);
+        gender.setOutputMarkupId(true);
+
+        AjaxButton button = new AjaxButton("submit") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                super.onSubmit(target, form);
+                text.setEnabled(false);
+                gender.setEnabled(false);
+
+                target.add(text);
+                target.add(gender);
+            }
+
+        };
+       /* Button button = new Button("submit"){
+            @Override
+            public void onSubmit() {
+                super.onSubmit();
+                System.out.println("Name :" + user.getName());
+                System.out.println("Gender :" + user.getGender());
+            }
+        };*/
+        add(form);
+        form.add(text);
+        form.add(gender);
+        form.add(button);
+    }
+}
