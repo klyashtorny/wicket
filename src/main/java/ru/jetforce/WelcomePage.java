58282995030f85444efa4fb5c1c388ad41e09a3e
Anton Kliashtorny <lm_1@tut.by>
2018-02-22 14:23:31 +0300

    implement AjaxButton, WicketAjaxDebug

--- src/main/java/ru/jetforce/WelcomePage.java
+++ src/main/java/ru/jetforce/WelcomePage.java
@@ -1,7 +1,8 @@
 package ru.jetforce;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
@@ -20,18 +21,32 @@ public class WelcomePage extends WebPage implements Serializable {
         genderChoices.add("Female");
         final User user = new User();
         Form<?> form = new Form("form");
-        TextField<String> text = new TextField<String>("text", new PropertyModel<String>(user, "name"));
-        DropDownChoice<String> gender = new DropDownChoice<String>("gender",
+        final TextField<String> text = new TextField<String>("text", new PropertyModel<String>(user, "name"));
+        text.setOutputMarkupId(true);
+        final DropDownChoice<String> gender = new DropDownChoice<String>("gender",
                 new PropertyModel<String>(user, "gender"), genderChoices);
+        gender.setOutputMarkupId(true);
 
-        Button button = new Button("submit"){
+        AjaxButton button = new AjaxButton("submit") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                super.onSubmit(target, form);
+                text.setEnabled(false);
+                gender.setEnabled(false);
+
+                target.add(text);
+                target.add(gender);
+            }
+
+        };
+       /* Button button = new Button("submit"){
             @Override
             public void onSubmit() {
                 super.onSubmit();
                 System.out.println("Name :" + user.getName());
                 System.out.println("Gender :" + user.getGender());
             }
-        };
+        };*/
         add(form);
         form.add(text);
         form.add(gender);
